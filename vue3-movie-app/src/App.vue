3ac5405e59d1050d06717ca82c571d6c28494290
kwonhyukil <gurdlf320@naver.com>
2025-02-27 16:08:33 +0900

    Movie 동기, 비동기 연습

--- vue3-movie-app/src/App.vue
+++ vue3-movie-app/src/App.vue
@@ -1,14 +1,17 @@
 <template>
   <Headera />
   <router-view />
+  <Footera />
 </template>
 
 <script>
 import Headera from './components/HeaderPage.vue'
+import Footera from './components/FooterPage.vue'
 
 export default {
   components: {
-    Headera
+    Headera,
+    Footera
   }
 }
 </script>
